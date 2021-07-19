31cfd9c41fc158c6c44b1f92b1a58338af0fbfb3
Rahul1o1 <87667687+Rahul1o1@users.noreply.github.com>
2021-07-20 02:31:19 +0530

    Add files via upload

---
+++ title.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+function func(){
+    return(
+        <div>
+            <div>School</div>
+            <div>Friends</div>
+        </div>
+    )
+}
+
+export default func;
